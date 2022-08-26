489bf41b54e2602a33ce667d586a01c242ab821c
Waleed Nasir <waleednasir676@gmail.com>
2022-08-26 23:48:07 +0500

    Fixed

--- src/App/layout/AdminLayout/NavBar/NavRight/index.js
+++ src/App/layout/AdminLayout/NavBar/NavRight/index.js
@@ -89,7 +89,7 @@ class NavRight extends Component {
                             <Dropdown.Menu alignRight className="profile-notification">
                                 <div className="pro-head">
                                     <img src={Avatar1} className="img-radius" alt="User Profile" />
-                                    <span>{userDetails.influencer_name}</span>
+                                    <span>{userDetails?.influencer_name}</span>
                                     <a href={'/signin'} className="dud-logout" title="Logout">
                                         <i className="feather icon-log-out" />
                                     </a>
